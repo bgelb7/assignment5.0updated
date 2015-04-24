13ceabf01f9088f755150dd531993c2ff9829a3c
bgelb7 <bengelb7@gmail.com>
2015-04-24 11:06:14 -0400

    updated 4/24 changes
    Make sure pages still pass validation

---
+++ header.php
@@ -0,0 +1,21 @@
+<div id="wrapper">
+  <header>
+          <h1 id="logo">Pho Hồng</h1>
+          <nav> 
+              <a href="index.php">
+              <span class="highlight">Home</span></a>
+              <a href="intro.php">
+              <span class="highlight">Intro</span></a>
+              <a href="menu.php">
+              <span class="highlight">Menu</span></a>
+              <a href="history.php">
+              <span class="highlight">History</span></a>
+              <a href="form.php">
+              <span class="highlight">Service Feedback</span></a>
+        
+              <a href="form2.php">
+              <span class="highlight">Contact Us</span></a>
+              
+          </nav>
+  </header>
+</div>
---
+++ intro.php
@@ -0,0 +1,15 @@
+<?php
+include "top.php";
+
+
+?>
+<div id="box"> <h1 id="introid">Introduction</h1>
+<p class="intropage">Pho Hong has been around for 20 years.  Founded by Vietnamese immigrants, it became a hotspot for a quick and delicious meal. It is described in social media as a "Popular BYOB spot for Vietnamese noodle soups & other traditional dishes in a casual setting"(google.com). Pho Hong is a small family owned and operated restaurant, so if your order takes too long, please be patient. </p>
+<figure class="intropageimg">
+    <img class="intro" alt="Service." src="images/intro.jpg"></figure>  </div><!-- taken from http://finedininglovers.cdn.crosscast-system.com/BlogPost/xl_7051_TP-pho-soup-finedininglovers.jpg-->
+<?php
+include "footer.php";
+
+
+
+
